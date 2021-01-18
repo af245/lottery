9ce0cfe8db17cbdcf7db36d5d97d897ce104bdcf
Andres <camilofrnc182@gmail.com>
2021-01-17 21:10:47 -0500

    branch commit

--- src/Lottery.js
+++ src/Lottery.js
@@ -2,34 +2,34 @@ import React from 'react';
 import './Lottery.css';
 import LotteryBall from './LotteryBall';
 
-class Lottery extends React.Component{
-static defaultProps ={
-    title: "Powerball",
-    numBalls: 6,
-    maxNum: 40
-}
+class Lottery extends React.Component {
+    static defaultProps = {
+        title: "Powerball",
+        numBalls: 6,
+        maxNum: 40
+    }
 
-state = { nums: Array.from({length: this.props.numBalls})}; //Array.from assigns empty array according to the length
+    state = { nums: Array.from({ length: this.props.numBalls }) }; //Array.from assigns empty array according to the length
 
-generate = e =>{
+    generate = e => {
 
-    this.setState(curState => ({
-        nums: curState.nums.map(n => Math.floor(Math.random()*this.props.maxNum)+1) //loops and changes the state of nums
-    }));
+        this.setState(curState => ({
+            nums: curState.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1) //loops and changes the state of nums
+        }));
 
-}
-    render(){
-        return(
+    }
+    render() {
+        return (
             <div className="container">
                 <div className="title">
                     <h3>{this.props.title}</h3>
                 </div>
                 <div className="LottoBalls">
-                    {this.state.nums.map(n =>   
-                        <LotteryBall num={n}/>  //loops+renders through the ball component
-                        
+                    {this.state.nums.map(n =>
+                        <LotteryBall num={n} />  //loops+renders through the ball component
+
                     )}
-    
+
                 </div>
 
                 <button onClick={this.generate}>
